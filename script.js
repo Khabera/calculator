caab4a322ff3066d511ed80d158665f956fa5f2e
Shenmew <Cessnator007@gmail.com>
2021-11-12 16:36:12 -0800

    Code Cleanup and Bug Fixes

--- script.js
+++ script.js
@@ -10,172 +10,132 @@ function divide(a,b){
 function subtract(a,b){
     return (a-b);
 }
-//what happens when you press the = key
 
-//take 2+ inputs, and what operations to perform on them
 const numberButtons = document.querySelectorAll('.number-button');
 const outputLine = document.querySelector('#output-screen')
 const runningEquation = document.querySelector('#running-equation');
-let waitingForInput = true;
+let onOperator = true;
 let num = '';
-let outputGiven = false
+let onOutput = false
 let cleared=true;
 let memory=0;
 let darkMode = false;
+console.log(numberButtons);
 numberButtons.forEach(button => {
     button.addEventListener('click', function(){
         if(button.getAttribute('number')=='.' && num.includes('.')){
             return;
             }
-        if(outputGiven==true && waitingForInput==false){
-            console.log('here');
+        if(onOutput==true && onOperator==false){
             num='';
             inputArray = [];
         }
-        waitingForInput = false;
-        if(outputGiven==true){
-            console.log(inputArray);
-            num=inputArray[0];
-            outputLine.textContent = '';
-            runningEquation.textContent=inputArray[0];
-            outputGiven=false;
+        if(onOutput==true){
             num='';
         }
+        onOperator = false;
+        onOutput=false;
         cleared=false;
-        num = +`${num}${button.getAttribute('number')}`;
-        outputLine.textContent = num;
-        runningEquation.textContent = `${inputArray.join(' ')} ${num}`;
+        num = (`${num}${button.getAttribute('number')}`);
+        console.log(button.getAttribute('number'))
+
+        pushDisplay();
         })
 })
+function pushDisplay(){
+    outputLine.textContent = num;
+    runningEquation.textContent = `${inputArray.join(' ')} ${num}`;
+}
 
-//seperate "set inputs" function?
-//designate operator and get new number
 const operatorButtons = document.querySelector('#operator-buttons').querySelectorAll('button');
-console.log(operatorButtons);
-let operator;
+let operation;
 operatorButtons.forEach(button =>{
     button.addEventListener('click', function(){
         operation = button.getAttribute('operation');
+        if(operation=='plusminus'){
+            if(onOutput||onOperator){
+                return;
+            }
+            num=-num
+            pushDisplay();
+            return;
+            }
+        if((!onOperator)&&(!onOutput)){
+            inputArray.push(parseFloat(num));
+            }
         if(operation=='='){
+            if(!onOperator){
             operate();
-        }else if(operation == 'plusminus'){
-            num=-num;
-            outputLine.textContent = num;
-        }else if(waitingForInput == false){
-                //console.log(operation);
-                designateOperator();
-            
-        }
+            }
+        }else{
+            designateOperator();  
+            }          
+        pushDisplay();
     })
 })
 let inputArray = [];
 function designateOperator(){
-    if(outputGiven==false){
-    inputArray.push(num);}
-    inputArray.push(operation);
-    //console.log(inputArray);
-    //console.log(inputArray.indexOf('+'))
+    if(onOperator==true){
+        inputArray.pop();
+    }
+    inputArray.push(operation); 
     num = '';
-    waitingForInput = true;
-    runningEquation.textContent=inputArray.join(' ');
+    onOperator = true;
 }
 
 function operate(){
-    inputArray.push(+num);
-    runningEquation.textContent=inputArray.join(' ');
-    console.log(inputArray);
-
-    let multIndex = inputArray.indexOf('×');
-    while(multIndex>0){
-        console.log(multIndex);
-        a=inputArray[multIndex-1];
-        b=inputArray[multIndex+1];
-        product = multiply(a,b);
-        inputArray.splice((multIndex-1), 3, product)
-        console.log(inputArray);
-        multIndex = inputArray.indexOf('×');
+    while(inputArray.length>1){
+        inputArray.forEach(function(item){
+            let index = inputArray.indexOf(`${item}`);
+            if(item == '+'){
+                feedback = add(inputArray[index-1], inputArray[index+1]);
+            }else if(item == '-'){
+                feedback = subtract(inputArray[index-1], inputArray[index+1]);
+            }else if(item == '×'){
+                feedback = multiply(inputArray[index-1], inputArray[index+1]);
+            }else if (item == '÷'){
+                feedback = divide(inputArray[index-1], inputArray[index+1]);
+            }else{
+                return;
+            }
+            inputArray.splice((index-1), 3, feedback);
+        });
     }
-
-    let divIndex = inputArray.indexOf('÷');
-    while(divIndex>0){
-        console.log(divIndex);
-        a=inputArray[divIndex-1];
-        b=inputArray[divIndex+1];
-        product = divide(a,b);
-        inputArray.splice((divIndex-1), 3, product)
-        console.log(inputArray);
-        divIndex = inputArray.indexOf('÷');
-    }
-
-    let addIndex = inputArray.indexOf('+');
-    while(addIndex>0){
-        console.log(addIndex);
-        a=inputArray[addIndex-1];
-        b=inputArray[addIndex+1];
-        product = add(a,b);
-        inputArray.splice((addIndex-1), 3, product)
-        console.log(inputArray);
-        addIndex = inputArray.indexOf('+');
-    }
-
-    let subtIndex = inputArray.indexOf('-');
-    while(subtIndex>0){
-        console.log(subtIndex);
-        a=inputArray[subtIndex-1];
-        b=inputArray[subtIndex+1];
-        product = subtract(a,b);
-        inputArray.splice((subtIndex-1), 3, product)
-        console.log(inputArray);
-        subtIndex = inputArray.indexOf('-');
-    }
-    outputGiven=true;
-    outputLine.textContent = inputArray[0];
+    onOutput=true;
+    num='';
+    pushDisplay();
 }
 document.querySelector('#clear').addEventListener('click', function(){
     num = '';
-    outputLine.textContent = num;
-    runningEquation.textContent = `${inputArray.join(' ')} ${num}`;
-    waitingForInput = true;
+    onOperator = true;
     if(cleared==true){
         inputArray=[];
-        runningEquation.textContent = `${inputArray.join(' ')} ${num}`;
     }
     cleared=true;
-    if(outputGiven==true){
-    waitingForInput=false;
+    if(onOutput==true){
+        inputArray=[];
+        onOutput=false;
     }
+    pushDisplay();
 });
 document.querySelector('#memset').addEventListener('click', function(){
     memory=num;
-    console.log(memory);
 });
 document.querySelector('#memrecall').addEventListener('click', function(){
     num=memory;
-    runningEquation.textContent = `${inputArray.join(' ')} ${num}`;
-    outputLine.textContent = num;
+    pushDisplay();
 });
 document.querySelector('#onoff').addEventListener('click', function(){
+    let coloredNodes = document.querySelectorAll('.body, #output-screen, #running-equation, #calc-body, button');
     if(darkMode==false){
-        document.querySelector('body').classList.add('class', 'dark')
-        document.querySelector('#output-screen').classList.add('class', 'dark')
-        document.querySelector('#running-equation').classList.add('class', 'dark')
-        document.querySelector('#calc-body').classList.add('class', 'dark')
-        document.querySelectorAll('button').forEach(button => {
+        coloredNodes.forEach(button => {
         button.classList.add('class', 'dark')
-    });}
+        })
+    }
     if(darkMode==true){
-        document.querySelector('body').classList.remove('class', 'dark')
-        document.querySelector('#output-screen').classList.remove('class', 'dark')
-        document.querySelector('#running-equation').classList.remove('class', 'dark')
-        document.querySelector('#calc-body').classList.remove('class', 'dark')
-        document.querySelectorAll('button').forEach(button => {
+        coloredNodes.forEach(button => {
         button.classList.remove('class', 'dark')
-    });
+        })
     }
     darkMode=!darkMode;
 })
-
-    //bug: pressing equal button adds to array
-    //set it so if you press another operator while there is no input, it switches operator
-    //if pressing number after output given, clear
-    //add cutting off numbers and high number functionality
